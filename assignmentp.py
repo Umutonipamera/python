2357e66eb44e0d5f3e28e382f4531b87164b6f8f
Umutonipamera <umutonipamera@gmail.com>
2021-03-28 16:53:43 +0300

    python

---
+++ assignmentp.py
@@ -0,0 +1,20 @@
+Countries=["kenya","Uganda","Rwanda","South Sudan"]
+Countries[2]
+Countries[1:3]
+Countriesfromabroad=["Burundi","Tanzania"]
+Countries.extend(Countriesfromabroad)
+print(Countries)
+Countries.reverse()
+print(Countries)
+a=[[1,2,3],[4,5,6],[7,8,9]]
+b=[numbers for sublist in a for numbers in sublist]
+print(b)
+c=[numbers%2 for numbers in b]
+print(c)
+
+
+            
+            
+            
+
+    
